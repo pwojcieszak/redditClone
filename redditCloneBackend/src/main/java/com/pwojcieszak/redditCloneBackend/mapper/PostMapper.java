df6e809077e8a96c4d70e8eb2a1c0fc3429c0558
pwojcieszak <piotrekw1002@gmail.com>
2024-02-11 16:08:29 +0100

    Implemented API to manage Votes

--- redditCloneBackend/src/main/java/com/pwojcieszak/redditCloneBackend/mapper/PostMapper.java
+++ redditCloneBackend/src/main/java/com/pwojcieszak/redditCloneBackend/mapper/PostMapper.java
@@ -1,24 +1,46 @@
 package com.pwojcieszak.redditCloneBackend.mapper;
 
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import com.pwojcieszak.redditCloneBackend.dto.PostRequest;
 import com.pwojcieszak.redditCloneBackend.dto.PostResponse;
 import com.pwojcieszak.redditCloneBackend.model.Post;
 import com.pwojcieszak.redditCloneBackend.model.Subreddit;
 import com.pwojcieszak.redditCloneBackend.model.User;
+import com.pwojcieszak.redditCloneBackend.repository.CommentRepository;
+import com.pwojcieszak.redditCloneBackend.repository.VoteRepository;
+import com.pwojcieszak.redditCloneBackend.service.AuthService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private VoteRepository voteRepository;
+    @Autowired
+    private AuthService authService;
+
     @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "description", source = "postRequest.description")
     @Mapping(target = "subreddit", source = "subreddit")
+    @Mapping(target = "voteCount", constant = "0")
     @Mapping(target = "user", source = "user")
-    Post map(PostRequest postRequest, Subreddit subreddit, User user);
+    public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
 
     @Mapping(target = "id", source = "postId")
     @Mapping(target = "subredditName", source = "subreddit.name")
     @Mapping(target = "userName", source = "user.username")
-    PostResponse mapToDto(Post post);
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    public abstract PostResponse mapToDto(Post post);
 
+    Integer commentCount(Post post) {
+        return commentRepository.findByPost(post).size();
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
 }
--- redditCloneBackend/src/main/java/com/pwojcieszak/redditCloneBackend/repository/VoteRepository.java
+++ redditCloneBackend/src/main/java/com/pwojcieszak/redditCloneBackend/repository/VoteRepository.java
@@ -1,9 +1,14 @@
 package com.pwojcieszak.redditCloneBackend.repository;
 
+import com.pwojcieszak.redditCloneBackend.model.Post;
+import com.pwojcieszak.redditCloneBackend.model.User;
 import com.pwojcieszak.redditCloneBackend.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 }
